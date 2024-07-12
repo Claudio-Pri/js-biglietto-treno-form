52b94eb9fe3ea90be4bc9f1234219019a286851a
Claudio-Pri <priv.claudio@gmail.com>
2024-07-12 17:33:58 +0200

    calcolo prezzo da input

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,68 @@
 
 // dichiarazione variabili
 
-const passengerAge = prompt("Qual'è la tua età?");
-//come faccio a dire che può accettare solo numeri?
-console.log('Età passeggero', passengerAge);
-
-const travelDistance = prompt('Quanti chilometri devi percorrere?');
-console.log('Distanza da percorrere', travelDistance, 'km');
 
 // prezzo al chilometro
 const pricePerKm = 0.21;
 console.log('pricePerKm', pricePerKm, '€');
 
-//prezzo base
-let prezzoBase = travelDistance * pricePerKm;
-console.log('prezzo base', prezzoBase, '€');
 
 let prezzoFinale;
 let sconto;
+let travelDistance;
 
-// ---modificare prima variabili con parseInt---
-if (isNaN(passengerAge) || isNaN(travelDistance)) {
-    alert('mannaggia a te');
-}
+const travelDistanceInNumbers = parseInt(travelDistance);
 
-else {
-    if (passengerAge < 18) {
-        sconto = ((prezzoBase / 100) * 20);
-        console.log('sconto', '20%', sconto);
-        prezzoFinale = prezzoBase - sconto;
+//variabile bottone
+const bottone = document.getElementById('submit-button');
+
+bottone.addEventListener('click',
+
+    function (event) {
+        const passengerAge = document.getElementById('age').value;
+        const passengerAgeInNumbers = parseInt(passengerAge);
+        console.log('Età', passengerAgeInNumbers);
+
+        const travelDistance = document.getElementById('travel-distance').value;
+        const travelDistanceInNumbers = parseInt(travelDistance);
+        console.log('KM', travelDistanceInNumbers);
+
+        event.preventDefault();
+        console.log('event', event, typeof event)
+
+        //prezzo base
+        let prezzoBase = travelDistanceInNumbers * pricePerKm;
+        console.log('prezzo base', prezzoBase, '€');
+
+        // ---modificare prima variabili con parseInt---
+        if (isNaN(passengerAgeInNumbers) || isNaN(travelDistanceInNumbers)) {
+            alert('mannaggia a te');
+        }
+
+        else {
+            if (passengerAgeInNumbers < 18) {
+                sconto = ((prezzoBase / 100) * 20);
+                console.log('sconto', '20%', sconto);
+                prezzoFinale = prezzoBase - sconto;
+            }
+            else if (passengerAgeInNumbers >= 65) {
+                sconto = ((prezzoBase / 100) * 40);
+                console.log('sconto', '40%', sconto);
+                prezzoFinale = prezzoBase - sconto;
+            }
+
+            else {
+                prezzoFinale = prezzoBase;
+                console.log('Prezzo intero', prezzoFinale);
+            }
+
+            document.getElementById('prezzo-biglietto').innerHTML = '€' + prezzoFinale.toFixed(2);
+
+        }
     }
-    else if (passengerAge >= 65) {
-        sconto = ((prezzoBase / 100) * 40);
-        console.log('sconto', '40%', sconto);
-        prezzoFinale = prezzoBase - sconto;
-    }
+)
 
-    else {
-        prezzoFinale = prezzoBase;
-        console.log('Prezzo intero', prezzoFinale);
-    }
 
-    document.getElementById('prezzo-biglietto').innerHTML = '€' + prezzoFinale.toFixed(2);
-
-}
 
 
 
